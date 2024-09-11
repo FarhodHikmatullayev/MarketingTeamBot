4deb5d6dae95f45cb384159e5e15a0e50bcd43ad
Farhodjon <farhodjonhikmatullayev@gmail.com>
2024-09-11 16:56:05 +0500

    modified

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -39,12 +39,18 @@ async def get_contact(message: Message):
 @dp.message_handler(CommandStart(), state='*')
 async def bot_start(message: types.Message, state: FSMContext):
     print('user_telegram_id', message.from_user.id)
-
+    users = await db.select_users(telegram_id=message.from_user.id)
     # await bot.send_message(chat_id=-1002210692356, text="chat_id_topildi")
+    if not users:
+        text = f"Salom, {message.from_user.full_name}!\n"
+        text += "Botimizga xush kelibsiz\n" \
+                "Botdan ro'yxatdan o'tish uchun kontaktingizni yuboring"
 
-    text = f"Salom, {message.from_user.full_name}!\n"
-    text += "Botimizga xush kelibsiz\n" \
-            "Botdan ro'yxatdan o'tish uchun kontaktingizni yuboring"
-
-    await message.answer(text, reply_markup=keyboard)
+        await message.answer(text, reply_markup=keyboard)
+    else:
+        text = f"Salom, {message.from_user.full_name}!\n"
+        text += "Botimizga xush kelibsiz\n" \
+                "Quyidagi bo'limlardan birini tanlang"
+        markup = await menu_keyboard(user_id=message.from_user.id)
+        await message.answer(text=text, reply_markup=markup)
     await state.finish()
